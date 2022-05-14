c29650a8f4a43b66500aadea3e03227d6e570c7d
Jung Gyu Yoon <yjg30737@gmail.com>
2022-05-14 18:31:19 +0900

    Update setup.py to upload this to pypi

--- setup.py
+++ setup.py
@@ -1,14 +1,24 @@
+import codecs
+import os
+
 from setuptools import setup, find_packages
+
+here = os.path.abspath(os.path.dirname(__file__))
+
+with codecs.open(os.path.join(here, "README.md"), encoding="utf-8") as fh:
+    long_description = "\n" + fh.read()
 
 setup(
     name='pyqt-file-list-widget',
-    version='0.0.2',
+    version='0.0.1',
     author='Jung Gyu Yoon',
     author_email='yjg30737@gmail.com',
     license='MIT',
     packages=find_packages(),
     description='PyQt QListWidget for files (being able to drop the files based on user-defined extensions)',
     url='https://github.com/yjg30737/pyqt-file-list-widget.git',
+    long_description_content_type='text/markdown',
+    long_description=long_description,
     install_requires=[
         'PyQt5>=5.8',
         'pyqt-tooltip-list-widget>=0.0.1',
